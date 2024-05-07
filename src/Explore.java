bb4b3b3f1250c1b2fc14db1919f109af8656cbe2
Z3NA <zanaboss@live.dk>
2024-05-07 17:00:49 +0200

    fishing, item er done - og random dialog.

--- src/Explore.java
+++ src/Explore.java
@@ -94,25 +94,109 @@ public class Explore {
 
 
     public void randomDialogue() {
-
-        Scanner scanner = new Scanner(System.in);
-
-        int delay = (1000);
         System.out.println("Exploring the area... (for knowledge)");
+
         int randy = random(100);
+
         if (randy < 20) {
-            System.out.println("You meet a random PokeTrainee..");
-            System.out.println(" ");
-            System.out.println("I want to become Strong... but i can't seem to catch any pokemons.. what do i do!");
-            String userInput = scanner.nextLine();
-            System.out.println(userInput + "Hmm.. never thought about that, I'll do my best!");
-
+            dialogGirlyTrainee();
+        } else if (randy < 40) {
+            dialogFishingMan();
+        } else if (randy < 60) {
+            dialogIdiotKid();
+        } else if (randy < 80) {
+            dialogOldman();
+        } else if (randy < 95) {
+            dialogLegendary();
         }
-
 
     }
 
+    public void dialogLegendary() {
+        Scanner scanner = new Scanner(System.in);
+        ui.displayMsg("You are enjoying yourself and see a group of people storming at you!");
+        ui.displayMsg("It seems like you need to prepare for battle");
+        System.out.println("Some guy named 'Red' wants to battle you");
+        ui.displayMsg("Do you want this. y/n");
+        ui.userInput();
+        if (scanner.nextLine().equals("y")) {
+            //Battle red
+        } else if (scanner.nextLine().equals("n")) {
+            System.out.println("Next time, take the battle, you wont regret it..Goodluck");
+            //todo enten tilbage til randomdialog eller menu
+            randomDialogue();
+        }
 
+    }
+
+    public void dialogOldman() {
+        Scanner scanner = new Scanner(System.in);
+        ui.displayMsg("You see a old man near a statue...");
+        ui.displayMsg("Do you want to approach him? y/n");
+        ui.userInput();
+        if (scanner.nextLine().equals("y")) {
+            System.out.println("Soon this beast will come back alive.");
+            System.out.println("i hope there is good enough poketrainers in this world..");
+            System.out.println("I heard last time it came, the world was near extinction");
+            System.out.println("But one unbelievable savior came, and harnest the darkness. no one knows what happend to him or the beast.. ");
+            System.out.println("But that was 600 years ago, what do we do now!...");
+            System.out.println("faith... faith.. thats all, faith.");
+        } else if (scanner.nextLine().equals("n")) {
+            System.out.println("He looked worried..maybe i should talk to him.");
+            //todo enten tilbage til randomdialog eller menu
+            randomDialogue();
+        }
+    }
+
+
+    public void dialogIdiotKid() {
+        Scanner scanner = new Scanner(System.in);
+        ui.displayMsg("You see a kid sitting near a tree...");
+        ui.displayMsg("Do you want to approach him? y/n");
+        ui.userInput();
+        if (scanner.nextLine().equals("y")) {
+            ui.displayMsg("When i think about it, you, too, are all alone in this world..");
+        } else if (scanner.nextLine().equals("n")) {
+            System.out.println("Trust me, you dodged a bullet..");
+            //todo enten tilbage til randomdialog eller menu
+            randomDialogue();
+        }
+    }
+
+    public void dialogFishingMan() {
+        Scanner scanner = new Scanner(System.in);
+        ui.displayMsg("You see a man fishing...");
+        ui.displayMsg("do you want to approach him? y/n");
+        ui.userInput();
+        if (scanner.nextLine().equals("y")) {
+            System.out.println("These lakes has a lot of history in them\n");
+            System.out.println("There was a legend about a big fish..\n");
+            System.out.println("Tales says that the one, that gets the hold of it, will become a strong poketrainer");
+            System.out.println("it was about this season it should appear...");
+            System.out.println("So far i have only found magikarps, but one day i might see it!");
+            //todo enten tilbage til randomdialog eller menu
+        } else if (scanner.nextLine().equals("n")) {
+            System.out.println("No sushi today..");
+            randomDialogue();
+        }
+    }
+
+    public void dialogGirlyTrainee() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You meet a random PokeTrainee..");
+        ui.displayMsg("do you want to approach her? y/n");
+        ui.userInput();
+        if (scanner.nextLine().equals("y")) {
+            System.out.println("I want to become Strong... but i can't seem to catch any pokemons.. what do i do!");
+            String userInput = scanner.nextLine();
+            System.out.println(userInput + "Hmm.. never thought about that, I'll do my best!");
+            //todo enten tilbage til randomdialog eller menu
+        } else if (scanner.nextLine().equals("n")) {
+            System.out.println("You fled the area");
+            randomDialogue();
+        }
+
+    }
 
     public void showMagikarp(int delay) throws InterruptedException {
 
