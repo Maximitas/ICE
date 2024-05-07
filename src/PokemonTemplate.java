654684d9661d72204d8247d18e34b6d7e7f8fdb3
AerrowV <businessasimk@gmail.com>
2024-05-07 12:54:04 +0200

    Added attributes to PokemonTemplate and csv for pokemon data

--- src/PokemonTemplate.java
+++ src/PokemonTemplate.java
@@ -1,2 +1,22 @@
 public interface PokemonTemplate {
+
+    int pokemonID();
+
+    int hp();
+
+    int lvl();
+
+    int attackPower();
+
+    int defensePower();
+
+    String name();
+
+    String typeOne();
+
+    String typeTwo();
+
+    String abilities();
+
+    String rarity();
 }
