463ff6376cb536bb8b86bab96f1db96a56fb57f1
Z3NA <zanaboss@live.dk>
2024-05-10 14:59:43 +0200

    QoL update

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 Explore explore = new Explore();
        FileIO file = new FileIO();
-  file.loadPokemonFromFile("Data\\Pokemon.csv");
+  //file.loadPokemonFromFile("Data\\Pokemon.csv");
 
         UserInterface userInterface = new UserInterface();
 
