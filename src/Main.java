12b38ac8d9bb18c82a488befcfdcdad4eeb9099f
Z3NA <zanaboss@live.dk>
2024-05-11 14:55:12 +0200

    Musiccc

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
+Music music = new Music();
+Music.playMusic("""
+        src/Pokémon-Theme-Song.wav""");
 
         UserInterface userInterface = new UserInterface();
-
         userInterface.startMenu();
 
     }
---
+++ src/Music.java
@@ -0,0 +1,21 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Music {
+    public static void main(String[] args) {
+        playMusic("src/Pokémon-Theme-Song.wav"); // Replace with the path to your sound file
+    }
+    public static void playMusic(String filePath) {
+        try {
+            File soundFile = new File(filePath);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
