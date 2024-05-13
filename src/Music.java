15604feb8ec27b52b21ba4333005b16fbf1d380c
Z3NA <zanaboss@live.dk>
2024-05-13 11:43:54 +0200

    Music added

--- src/Music.java
+++ src/Music.java
@@ -4,10 +4,36 @@ import java.io.IOException;
 
 public class Music {
     public static void main(String[] args) {
-        playMusic("src/Pokémon-Theme-Song.wav"); // Replace with the path to your sound file
+        playMusic("src/Pokémon-Theme-Song.wav");// Replace with the path to your sound file
+        playMusicBattle("src/pokemon-battle.wav");
+        playMusicStart("src/vN8GiDHbO4.wav");
     }
 
+
     public static void playMusic(String filePath) {
+        try {
+            File soundFile = new File(filePath);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void playMusicBattle(String filePath) {
+        try {
+            File soundFile = new File(filePath);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void playMusicStart(String filePath) {
         try {
             File soundFile = new File(filePath);
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
