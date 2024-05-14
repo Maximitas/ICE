20f42ee7960d13c3269dd5e8a3476aa8073cf150
ash2213 <157792270+ash2213@users.noreply.github.com>
2024-05-14 12:55:40 +0200

    Main and class diagram fixed

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,10 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-Explore explore = new Explore();
-Player player = new Player();
-CombatInterface combatInterface = new CombatInterface();
-//combatInterface.combatOptionsDarkrai(new Player());
-       // explore.dialogOldman(player);
 
-        Music music = new Music();
-       // Music.playMusic("""
-       //        src/Pokémon-Theme-Song.wav""");
-       //Music.playMusic("src/pokemon-battle.wav");
         Music.playMusicStart("src/vN8GiDHbO4.wav");
-
-       // FileIO file = new FileIO();
-       // file.loadPokemonFromFile("Data\\Pokemon.csv");
-
         UserInterface userInterface = new UserInterface();
         userInterface.startMenu();
-      //  explore.gatesOfPokemon(player);
     }
 
 
